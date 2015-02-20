9e2d7e8b53454ae4c7c180fbb311f3dde145e48f
Ryan Williams <rwilliams@stream9.net>
2015-02-20 16:10:06 -0500

    updated schema sql file

--- tests/schemas/simple.sql
+++ tests/schemas/simple.sql
@@ -2,7 +2,7 @@ CREATE TABLE `parents` (
   `parent_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `parent_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `children` (
   `child_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -10,4 +10,4 @@ CREATE TABLE `children` (
   `child_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`child_id`),
   KEY `parent_id` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
